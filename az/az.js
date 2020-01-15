67f3bfb0078296d7e9a9cf6abaf2a7a4220fd0b3
Cak Sup <32424007+caksup@users.noreply.github.com>
2020-01-15 21:29:53 +0700

    Update az.js

--- az/az.js
+++ az/az.js
@@ -1,1 +1,1 @@
-document.write('<script src="https://caksup.github.io/nulisaksarajawa/az/css.js"></script>');
+document.write('<script src="https://caksup.github.io/nulisaksarajawa/az/css.js"></script>')
