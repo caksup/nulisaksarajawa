8ee6297b1c281c721644b18dff3fad7a25ff6c90
Cak Sup <32424007+caksup@users.noreply.github.com>
2020-01-15 17:58:08 +0700

    Create z.js

---
+++ az/z.js
@@ -0,0 +1,20 @@
+<script type="text/javascript">
+window.onload = function(){
+    document.body.innerHTML = 
+        '<li><a href="#" class="menulink">text</a></li>'
+        +'<li><a href="#" class="menulink">text</a></li>'
+        +'<li><a href="#" class="menulink">text</a><ul>'
+        +'<li><a href="#" class="sub">text</a><ul>'
+        +'<li class="topline"><a href="#">text</a></li>'
+        +'<li><a href="#">text </a></li><li><a href="#">text</a></li>'
+        +'<li><a href="#">text</a></li><li><a href="#">text</a></li>'
+        +'<li><a href="#"text</a></li><li><a href="#">text</a></li></ul>'
+        +'</li><li><a href="#" class="sub">text </a><ul>'
+        +'<li class="topline"><a href="#">text</a></li>'
+        +'<li><a href="#">text</a></li></ul></li>'
+        +'<li><a href="#" class="sub">text</a></li>'
+        +'<li><a href="#" class="sub">text</a></li>'
+        +'</ul></li><li><a href="#" class="menulink">text</a></li>'
+        +'<li><a href="#" class="menulink">text</a>';
+
+}
