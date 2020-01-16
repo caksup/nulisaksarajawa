228c9522faca63556feb1f158d5917195dbbd672
Cak Sup <32424007+caksup@users.noreply.github.com>
2020-01-16 23:35:55 +0700

    Update css.js

--- az/css.js
+++ az/css.js
@@ -1,5 +1,5 @@
-document.write('<script src="https://caksup.github.io/nulisaksarajawa/az/css.js"></script>')
-document.write('<link rel="stylesheet" href="https://caksup.github.io/nulisaksarajawa/css/bootstrap/4.4.1/bootstrap.min.css">')
-document.write('<link rel="stylesheet" href="https://caksup.github.io/nulisaksarajawa/css/cs/gayane.css">')
-document.write('<link rel="stylesheet" href="https://caksup.github.io/nulisaksarajawa/css/naj/specimen_stylesheet.css" type="text/css" charset="utf-8" />')
-document.write('<link rel="stylesheet" href="https://caksup.github.io/nulisaksarajawa/css/naj/stylesheet.css" type="text/css" charset="utf-8" />')
+document.write('<link rel="stylesheet" href="https://caksup.github.io/nulisaksarajawa/css/bootstrap/4.4.1/bootstrap.min.css">' +
+'<link rel="stylesheet" href="https://caksup.github.io/nulisaksarajawa/css/caksup/1.0/caksup-style-v1.0.css">' +
+'<link rel="stylesheet" href="https://caksup.github.io/nulisaksarajawa/css/naj/specimen_stylesheet.css" type="text/css" charset="utf-8" />' +
+'<link rel="stylesheet" href="https://caksup.github.io/nulisaksarajawa/css/naj/stylesheet.css" type="text/css" charset="utf-8" />' +
+'<link rel="stylesheet" href="https://caksup.github.io/nulisaksarajawa/css/custom.css" type="text/css" charset="utf-8" />')
