3bef2565f26aa015087fac6accc4a3c4dfd69929
Cak Sup <32424007+caksup@users.noreply.github.com>
2020-01-17 00:09:59 +0700

    Update css.js

--- az/css.js
+++ az/css.js
@@ -1,8 +1,6 @@
-// Storing HTML code block in a variable
-var codeStore = '<link rel="stylesheet" href="https://caksup.github.io/nulisaksarajawa/css/bootstrap/4.4.1/bootstrap.min.css">' +
+// jancuk
+document.write('<link rel="stylesheet" href="https://caksup.github.io/nulisaksarajawa/css/bootstrap/4.4.1/bootstrap.min.css">' +
 				'<link rel="stylesheet" href="https://caksup.github.io/nulisaksarajawa/css/caksup/1.0/caksup-style-v1.0.css">' +
 				'<link rel="stylesheet" href="https://caksup.github.io/nulisaksarajawa/css/naj/specimen_stylesheet.css" type="text/css" charset="utf-8">' +
 				'<link rel="stylesheet" href="https://caksup.github.io/nulisaksarajawa/css/naj/stylesheet.css" type="text/css" charset="utf-8">' +
-				'<link rel="stylesheet" href="https://caksup.github.io/nulisaksarajawa/css/custom.css" type="text/css" charset="utf-8">';
-        // Inserting the code block to wrapper element
-document.getElementById("storecss").innerHTML = codeStore
+				'<link rel="stylesheet" href="https://caksup.github.io/nulisaksarajawa/css/custom.css" type="text/css" charset="utf-8">');
