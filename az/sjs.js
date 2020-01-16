01e9f127518e06460361bed65c066b063d32f4e3
Cak Sup <32424007+caksup@users.noreply.github.com>
2020-01-17 00:30:03 +0700

    Create sjs.js

---
+++ az/sjs.js
@@ -0,0 +1,13 @@
+//jancuk sikile
+document.write('<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>' +
+'<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>' +
+'<script src="https://caksup.github.io/nulisaksarajawa/js/bootstrap/4.4.1/bootstrap.min.js"></script>' +
+'<script src="https://caksup.github.io/nulisaksarajawa/js/bootstrap/4.4.1/bootstrap.bundle.min.js"></script>' +
+'<script src="https://unpkg.com/sweetalert2@7.0.6/dist/sweetalert2.all.js"></script>' +
+'<script src="https://kit.fontawesome.com/5ca0cd0796.js" crossorigin="anonymous"></script>' +
+'<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js" type="text/javascript" charset="utf-8"></script>' +
+'<script src="https://jv.wikipedia.org/w/index.php?title=Panganggo:Bennylin/trans.js&action=raw&ctype=text/javascript" type="text/javascript" language="javascript" charset="utf-8"></script>
+'<script src="https://caksup.github.io/nulisaksarajawa/js/naj/easytabs.js" typegerick="text/javascript" charset="utf-8"></script>
+'<script src="https://caksup.github.io/nulisaksarajawa/js/naj/transliterator.js" type="text/javascript" language="javascript" charset="utf-8"></script>
+'<script src="https://caksup.github.io/nulisaksarajawa/js/naj/script.js" type="text/javascript" language="javascript" charset="utf-8"></script>
+'<script id="dsq-count-scr" src="//kaksup.disqus.com/count.js" async></script>');
